59972a06d370f337df1df3f948abda55e6b6d7be
idbaliyah <idbaliyah@gmail.com>
2023-02-21 14:41:35 +0700

    First Commit

--- Java_Syntax/Main.java
+++ Java_Syntax/Main.java
@@ -1,7 +1,12 @@
 package Java_Syntax;
 
+// ---Create class Main---
+// ---A class should always start with an Uppercase first letter---
 public class Main {
+    // --Create method main---
     public static void main(String[] args) {
+        // ---use println() method to print a line of text to the screen----
+        // ---screen displaying sentences Learn java syntax---
         System.out.println("Learn java syntax");
     }
 }
